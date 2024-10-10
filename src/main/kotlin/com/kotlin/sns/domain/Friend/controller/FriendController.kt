bade6a624affc879a6ba9b0ad708e8ee2d7d41e6
attlet <tlsdbstjd124@naver.com>
2024-10-10 23:13:41 +0900

    feat : friend Controller 기본 작성

--- src/main/kotlin/com/kotlin/sns/domain/Friend/controller/FriendController.kt
+++ src/main/kotlin/com/kotlin/sns/domain/Friend/controller/FriendController.kt
@@ -1,11 +1,13 @@
 package com.kotlin.sns.domain.Friend.controller
 
 import com.kotlin.sns.domain.Friend.dto.request.RequestCreateFriendDto
+import com.kotlin.sns.domain.Friend.dto.request.RequestUpdateFriendDto
 import com.kotlin.sns.domain.Friend.dto.response.ResponseFriendDto
 import com.kotlin.sns.domain.Friend.service.FriendService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -23,16 +25,20 @@ class FriendController(
 ) {
     @GetMapping
     fun getFriendById(@RequestParam("friendId") friendId : Long) : ResponseFriendDto{
-
+        return friendService.findFriendById(friendId)
     }
 
     @PostMapping
-    fun createFriend(@RequestBody requestCreateFriendDto: RequestCreateFriendDto) : ResponseFriendDto{
-
+    fun sendFriend(@RequestBody requestCreateFriendDto: RequestCreateFriendDto) : ResponseFriendDto{
+        return friendService.sendFriend(requestCreateFriendDto)
     }
 
+    @PutMapping
+    fun updateFriend(@RequestBody requestUpdateFriendDto: RequestUpdateFriendDto) : ResponseFriendDto{
+        return friendService.updateFriend(requestUpdateFriendDto)
+    }
     @DeleteMapping
     fun deleteFriend(@RequestParam("friendId") friendId: Long){
-
+        friendService.deleteFriend(friendId)
     }
 }
