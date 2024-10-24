9365f27966c111a28cf5bce67c0f0a255f6199a7
attlet <tlsdbstjd124@naver.com>
2024-10-24 23:46:45 +0900

    feat : notification sse 추가
    notificationService subscribe, send 구현

--- src/main/kotlin/com/kotlin/sns/domain/Notification/controller/NotificationController.kt
+++ src/main/kotlin/com/kotlin/sns/domain/Notification/controller/NotificationController.kt
@@ -4,12 +4,14 @@ import com.kotlin.sns.domain.Notification.dto.request.RequestCreateNotificationD
 import com.kotlin.sns.domain.Notification.dto.response.ResponseNotificationDto
 import com.kotlin.sns.domain.Notification.entity.Notification
 import com.kotlin.sns.domain.Notification.service.NotificationService
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 
 @RestController
 @RequestMapping("/notify")
@@ -36,5 +38,10 @@ class NotificationController(
 
         return responseNotificationDtos
     }
+
+//    @GetMapping("/connect", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+//    fun SseConnect() : SseEmitter{
+//
+//    }
 
 }
--- src/main/kotlin/com/kotlin/sns/domain/Notification/service/NotificationService.kt
+++ src/main/kotlin/com/kotlin/sns/domain/Notification/service/NotificationService.kt
@@ -4,8 +4,11 @@ import com.kotlin.sns.domain.Notification.dto.request.RequestCreateNotificationD
 import com.kotlin.sns.domain.Notification.dto.response.ResponseNotificationDto
 import com.kotlin.sns.domain.Notification.entity.Notification
 import com.kotlin.sns.domain.Notification.entity.NotificationType
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 
 interface NotificationService {
     fun createNotification(requestCreateNotificationDto: RequestCreateNotificationDto)
     fun getNotificationsForUser(receiverId: Long): List<Notification>
+    fun subscribe(userId : Long) : SseEmitter
+    fun sendNotificationToClient(userId: Long, notification: Notification)
 }
