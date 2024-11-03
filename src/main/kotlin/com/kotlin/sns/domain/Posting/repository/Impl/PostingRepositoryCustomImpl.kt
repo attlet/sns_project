30f97af16b7a623258e16fe5bc1eda98dbeccf62
attlet <tlsdbstjd124@naver.com>
2024-11-03 23:33:02 +0900

    feat : getPostingList n + 1 이슈 해결

--- src/main/kotlin/com/kotlin/sns/domain/Posting/repository/Impl/PostingRepositoryCustomImpl.kt
+++ src/main/kotlin/com/kotlin/sns/domain/Posting/repository/Impl/PostingRepositoryCustomImpl.kt
@@ -1,5 +1,7 @@
 package com.kotlin.sns.domain.Posting.repository.Impl
 
+import com.kotlin.sns.domain.Comment.entity.QComment
+import com.kotlin.sns.domain.Member.entity.QMember
 import com.kotlin.sns.domain.Posting.entity.Posting
 import com.kotlin.sns.domain.Posting.entity.QPosting
 import com.kotlin.sns.domain.Posting.repository.PostingRepositoryCustom
@@ -12,9 +14,13 @@ class PostingRepositoryCustomImpl(
 ) : PostingRepositoryCustom {
 
     private val qPosting = QPosting.posting
+    private val qComment = QComment.comment
+    private val qMember = QMember.member
     override fun getPostingList(pageable: Pageable): List<Posting> {
         return jpaQueryFactory
             .selectFrom(qPosting)
+            .join(qPosting.comment, qComment).fetchJoin()
+            .join(qComment.member, qMember).fetchJoin()
 //            .where()
             .orderBy(qPosting.createdDt.desc())
             .offset(pageable.offset)
