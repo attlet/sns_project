b0e39514918e8af1a4af1e3530c662783bfdd06d
attlet <tlsdbstjd124@naver.com>
2024-12-15 21:52:31 +0900

    feat : logging 설정 운용 환경에 따라 분리 안 되는 현상 수정
    application-local, application-prod 파일 수정
    각 파일에 logback 파일 위치 명시함.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-logging")
 
 	//db 연동
 	implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2")
