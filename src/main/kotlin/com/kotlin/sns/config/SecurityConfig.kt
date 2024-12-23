a5768566ae2c56d03018fa76f585ac3c26bfdfa9
attlet <tlsdbstjd124@naver.com>
2024-12-23 22:34:35 +0900

    feat[#70] : spring actuator url 보안 해제

--- src/main/kotlin/com/kotlin/sns/config/SecurityConfig.kt
+++ src/main/kotlin/com/kotlin/sns/config/SecurityConfig.kt
@@ -34,7 +34,8 @@ class SecurityConfig (
         "/v3/api-docs/**",
         "/swagger-ui/**",
         "/swagger-resources/**",
-        "/auth/**")
+        "/auth/**",
+        "/api/actuator/**")
     @Bean
     fun filterChain(httpSecurity : HttpSecurity) : SecurityFilterChain {
         httpSecurity
