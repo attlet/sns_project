c21ac86ca15925edae96dcd0fedee936d70a657d
attlet <tlsdbstjd124@naver.com>
2024-12-27 00:09:31 +0900

    feat[#70] : security config에 actuator 경로 수정

--- src/main/kotlin/com/kotlin/sns/config/SecurityConfig.kt
+++ src/main/kotlin/com/kotlin/sns/config/SecurityConfig.kt
@@ -35,7 +35,7 @@ class SecurityConfig (
         "/swagger-ui/**",
         "/swagger-resources/**",
         "/auth/**",
-        "/actuator**")
+        "/actuator/**")
     @Bean
     fun filterChain(httpSecurity : HttpSecurity) : SecurityFilterChain {
         httpSecurity
