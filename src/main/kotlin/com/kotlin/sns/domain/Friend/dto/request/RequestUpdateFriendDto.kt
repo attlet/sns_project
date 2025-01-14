26e2fd93739cddcf6b9ef22b537c741afa8c044a
attlet <tlsdbstjd124@naver.com>
2025-01-14 22:37:13 +0900

    feat[#75] : 친구 요청, 수락 알람 기능 구현

--- src/main/kotlin/com/kotlin/sns/domain/Friend/dto/request/RequestUpdateFriendDto.kt
+++ src/main/kotlin/com/kotlin/sns/domain/Friend/dto/request/RequestUpdateFriendDto.kt
@@ -3,6 +3,7 @@ package com.kotlin.sns.domain.Friend.dto.request
 import com.kotlin.sns.domain.Friend.const.friendApplyStatusEnum
 
 class RequestUpdateFriendDto(
+    val friendRequestId : Long,
     val senderId : Long,
     val receiverId : Long,
     val status : friendApplyStatusEnum
--- src/main/kotlin/com/kotlin/sns/domain/Friend/dto/response/ResponseFriendDto.kt
+++ src/main/kotlin/com/kotlin/sns/domain/Friend/dto/response/ResponseFriendDto.kt
@@ -3,6 +3,7 @@ package com.kotlin.sns.domain.Friend.dto.response
 import com.kotlin.sns.domain.Friend.const.friendApplyStatusEnum
 
 class ResponseFriendDto(
+    val friendRequestId : Long,
     val senderId : Long,
     val receiverId : Long,
     val status : friendApplyStatusEnum
