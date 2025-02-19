cb57fec4c59c037f3b688af0ce3f99e016aac2ad
attlet <tlsdbstjd124@naver.com>
2025-02-19 23:41:25 +0900

    feat[#79] : memberService findMemberById 테스트 코드 작성

--- src/test/kotlin/com/kotlin/sns/domain/member/service/Impl/MemberServiceImplTest.kt
+++ src/test/kotlin/com/kotlin/sns/domain/member/service/Impl/MemberServiceImplTest.kt
@@ -2,20 +2,26 @@ package com.kotlin.sns.domain.member.service.Impl
 
 import com.kotlin.sns.domain.member.dto.response.ResponseMemberDto
 import com.kotlin.sns.domain.member.entity.Member
+import com.kotlin.sns.domain.member.repository.MemberRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.whenever
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.*
 
 @SpringBootTest
 class MemberServiceImplTest {
 
     @Autowired
     private lateinit var memberService: MemberServiceImpl
+    @Autowired
+    private lateinit var memberRepository : MemberRepository
     @Test
     fun findMemberByIdReturnsMemberWhenExists() {
         // given
+        val memberId : Long = 1L
 
         val member = Member(
             userId = "user123",
@@ -24,11 +30,12 @@ class MemberServiceImplTest {
             pw = "password",
             roles = listOf("USER")
         )
+
         val responseMember = ResponseMemberDto(name = "user123", "user1@example.com")
-//        whenever(memberRepository.findById(memberId)).thenReturn(Optional.of(member))
+        whenever(memberRepository.findById(memberId)).thenReturn(Optional.of(member))
 //        whenever(memberMapper.toDto(member)).thenReturn(responseMember)
 //
-//        // when
+        // when
         memberService.findMemberById(1L)
 
         // then
